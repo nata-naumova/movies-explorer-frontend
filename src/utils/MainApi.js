b152b5b2b76dcd730800225fe34e4f3e03c3f2b6
nata-naumova <naumonata@yandex.ru>
2022-12-07 16:35:05 +0300

    обновила ссылки 2

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,5 +1,5 @@
 //const BASE_URL = "http://localhost:3000";
-const BASE_URL = "http://movies-explorer.nata.nomoredomains.icu";
+const BASE_URL = "https://api.movies-explorer.nata.nomoredomains.icu";
 
 const getJson = (response) => {
   if (response.ok) {
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,5 +1,5 @@
 //export const BASE_URL = "http://localhost:3000";
-export const BASE_URL = "http://movies-explorer.nata.nomoredomains.icu";
+export const BASE_URL = "https://api.movies-explorer.nata.nomoredomains.icu";
 
 /* Регистрация пользователя */
 export const register = (data) => {
